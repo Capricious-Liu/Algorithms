e7c9ab4fcffc83a05a42f1a6402022718e083754
Capricious-Liu <1073715498@qq.com>
2019-02-10 00:13:17 +0800

    shell sort & merge sort

--- sort.cpp
+++ sort.cpp
@@ -55,12 +55,78 @@ vector<int> bubble_sort(vector<int>& array){
 
 // Insertion sort
 // 时间 n^2  空间 1
+vector<int> insert_sort(vector<int> & array){
+	for (int i = 1; i < array.size(); i++){
+		int pre_index = i - 1;
+		int cur_num = array[i];
+		while (pre_index >= 0 && cur_num > array[pre_index]){
+			array[pre_index + 1] = array[pre_index];
+			pre_index--;
+		}
+		array[pre_index+1] = cur_num;
+	}
+	return array;
+}
 
+// Shell Sort
+vector<int> shell_sort(vector<int>& array){
+	int gap = array.size()/3+1;
+	bool flag = false; // 保证gap为1时 会执行并且只执行一次，避免下方while进入死循环。
+	while (gap > 0){
+		for (int i = gap; i < array.size(); i++){
+			int tmp = array[i];
+			int j = i - gap;
+			for (; j>=0 && tmp>array[j]; j-=gap){
+				array[j + gap] = array[j];
+			}
+			array[j + gap] = tmp;
+		}
+		gap = gap / 3 + 1;
+		if (gap == 1 && !flag){
+			flag = true;
+		}
+		else if (gap == 1 && flag){
+			break;
+		}
+	}
+	return array;
+}
 
+// Merge Sort
+vector<int> merge(vector<int> left, vector<int> right){
+	vector<int> res;
+	int i=0, j = 0;
+	while (i < left.size() && j < right.size()){
+		if (left[i] > right[j]){
+			res.push_back(left[i]);
+			i++;
+		}
+		else{
+			res.push_back(right[j]);
+			j++;
+		}
+	}
+	while (i < left.size()){
+		res.push_back(left[i]);
+		i++;
+	}
+	while (j < right.size()){
+		res.push_back(right[j]);
+		j++;
+	}
+	return res;
+}
 
+vector<int> merge_sort(vector<int>& array){
+	if (array.size() == 1) return array;
+	int middle = array.size() / 2;
+	vector<int> left(array.begin(),array.begin()+middle);
+	vector<int> right(array.begin() + middle, array.end());
+	return merge(merge_sort(left), merge_sort(right));
+}
 
 
 int main(){
 	vector<int> array = { 4, 5, 2, 1, 3 };
-	cout << bubble_sort(array);
+	cout << merge_sort(array);
 }
