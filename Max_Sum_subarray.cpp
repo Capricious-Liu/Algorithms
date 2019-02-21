d89728d1b428c9879a8462845ca8cbc17c10dbf4
Capricious-Liu <1073715498@qq.com>
2019-02-21 21:34:15 +0800

    max_sum_subarray added

---
+++ Max_Sum_subarray.cpp
@@ -0,0 +1,41 @@
+/*
+Given an array,
+find the subarray which have the greatest sum
+*/
+
+
+#include"vector"
+#include"iostream"
+using namespace std;
+
+int FindGreatestSumOfSubArray(vector<int> array) {
+	int cur_max = INT_MIN;
+	int cur_sum = INT_MIN;
+	for (int i = 0; i<array.size(); i++){
+		if (cur_sum <= 0){
+			cur_sum = array[i];
+		}
+		else{
+			cur_sum += array[i];
+		}
+		if (cur_sum >= cur_max){
+			cur_max = cur_sum;
+		}
+	}
+	return cur_max;
+}
+
+/*
+Another solution:
+ Dynamic Programming
+ F£¨i£©=max£¨F£¨i-1£©+array[i] £¬ array[i]£©
+
+the same as the first solution
+*/
+
+
+
+int main(){
+	vector<int> a = { -1, 2, 3 };
+	cout << FindGreatestSumOfSubArray(a);
+}
